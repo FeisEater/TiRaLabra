e660d25f46d1b4320f4262521a15bdf2d58909fa
FeisEater <pasmpasm@cs.helsinki.fi>
2014-01-14 19:02:44 +0200

    muutettu maveniksi

---
+++ tiralabra/src/main/java/tiralabra/tiralabra/App.java
@@ -0,0 +1,13 @@
+package tiralabra.tiralabra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
